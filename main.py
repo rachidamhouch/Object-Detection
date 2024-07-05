af079acc32f3eb361fc51e7fe94750a3b7636714
Rachid Amhouch <ramhouch@c3r8p1.1337.ma>
2024-07-05 17:58:17 +0100

    better vertion

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 import cv2
 import cvzone
-from sort import *
 model = YOLO('../Yolo-Weights/yolov8n.pt')
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
+
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
@@ -17,27 +17,21 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
               "teddy bear", "hair drier", "toothbrush"
               ]
-
-tracker = Sort(20,)
-dtetctions = np.empty((0,5))
 while True:
     success,img = cap.read()
-    results = model(img, stream=True)
-    for r in results:
-        for box in r.boxes:
+    results = model.track(img, persist=True)
+    for detection in results[0]:
+        for box in detection.boxes:
             cls = int(box.cls[0])
+            id = int(box.id[0])
             conf = int(box.conf[0] * 100)
             if cls == 0 and conf > 50:
                 x1,y1,x2,y2 = box.xyxy[0]
                 x1,y1,x2,y2 = int(x1), int(y1), int(x2), int(y2)
                 w,h = x2-x1, y2-y1
-                dtetction = np.array([x1,y1,x2,y2,conf])
-                dtetctions = np.vstack((dtetctions, dtetction))
                 cvzone.cornerRect(img, (x1, y1, w, h))
-                cvzone.putTextRect(img,f"{classNames[cls]} {conf}%".upper(), (x1 + 5, max(y1-15, 18)), thickness=2,scale=1)
-    results = tracker.update(dtetctions)
-    for result in results:
-        print(result)
+                cvzone.putTextRect(img,f"{classNames[cls]} {conf}%, ID: {id}".upper(), (x1 + 5, max(y1-15, 18)), thickness=2,scale=1)
     cv2.imshow('xx', img)
     if cv2.waitKey(1) == ord('q'):
         break
+  
